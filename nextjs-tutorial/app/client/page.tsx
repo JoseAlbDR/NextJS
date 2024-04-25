799f6f4d1f0c7dce41f2eaf4d8715f93d7039e72
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-26 01:05:53 +0200

    feat: counter with use client in client component

--- nextjs-tutorial/app/client/page.tsx
+++ nextjs-tutorial/app/client/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const ClientPage = () => {
-  return <h1 className="text-7xl">ClientPage</h1>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-5xl font-semibold mb-8">Counter</h1>
+      <div className="flex items-center justify-start gap-4">
+        <button
+          className="btn btn-circle btn-primary"
+          onClick={() => setCounter((counter) => (counter -= 1))}
+        >
+          -
+        </button>
+        <div className="text-5xl ">{counter}</div>
+        <button
+          className="btn btn-circle btn-primary"
+          onClick={() => setCounter((counter) => (counter += 1))}
+        >
+          +
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ClientPage;
