a38a8bcee59bfdfe2a389216be15e2f9d28c0466
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-29 17:48:44 +0200

    feat: move getAllTasks logic to actions file

--- nextjs-tutorial/utils/actions.ts
+++ nextjs-tutorial/utils/actions.ts
@@ -1,9 +1,18 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import prisma from './db';
 
 export const createTaskAction = async (data: FormData) => {
   const content = data.get('content') as string;
 
   await prisma.task.create({ data: { content } });
+  revalidatePath('/tasks');
+};
+
+export const getAllTasksAtion = async () => {
+  const tasks = await prisma.task.findMany({
+    orderBy: { createdAt: 'desc' },
+  });
+  return tasks;
 };
