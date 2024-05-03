04242db3dc7589a609983c5cdd76164135e68b7c
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-03 17:14:07 +0200

    feat: Navigation/Sidebar complete

--- 02-my-dashboard/src/app/page.tsx
+++ 02-my-dashboard/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/dashboard/counter');
+  redirect('/dashboard/main');
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
