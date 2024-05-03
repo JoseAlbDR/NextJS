291af61ebead93788b97ba4c444114822a961892
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-03 17:28:59 +0200

    feat: CounterPage

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const CounterPage = () => {
-  return <div>CounterPage</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{counter}</span>
+      <div className="flex">
+        <button
+          onClick={() =>
+            setCounter((counter) => (counter >= 1 ? (counter -= 1) : counter))
+          }
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+        >
+          -1
+        </button>
+        <button
+          onClick={() => setCounter((counter) => (counter += 1))}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+        >
+          +1
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CounterPage;
