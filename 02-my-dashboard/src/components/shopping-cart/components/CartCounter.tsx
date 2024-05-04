71047cd6cf95daee10d291017146a0e023f835c0
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-04 19:41:17 +0200

    refactor: to use counterSlice with dispatch and selector

--- 02-my-dashboard/src/components/shopping-cart/components/CartCounter.tsx
+++ 02-my-dashboard/src/components/shopping-cart/components/CartCounter.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { decrement, increment } from '@/store/counter/counterSlice';
 import React, { useState } from 'react';
 
 interface Props {
@@ -6,21 +8,21 @@ interface Props {
 }
 
 const CartCounter = ({ value = 10 }: Props) => {
-  const [counter, setCounter] = useState(value);
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counterReducer.count);
+
   return (
     <>
-      <span className="text-9xl">{counter}</span>
+      <span className="text-9xl">{count}</span>
       <div className="flex">
         <button
-          onClick={() =>
-            setCounter((counter) => (counter >= 1 ? (counter -= 1) : counter))
-          }
+          onClick={() => dispatch(decrement())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           -1
         </button>
         <button
-          onClick={() => setCounter((counter) => (counter += 1))}
+          onClick={() => dispatch(increment())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
         >
           +1
