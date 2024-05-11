6d279d287062ad4d3b161c6dafc6b513a3611822
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-11 19:30:04 +0200

    feat: POST route

--- 03-admin-todos/src/app/api/todos/route.ts
+++ 03-admin-todos/src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/app/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server';
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -24,4 +25,23 @@ export async function GET(request: Request) {
   });
 
   return NextResponse.json(todos);
+}
+
+const postSchema = yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  try {
+    const { complete, description } = await postSchema.validate(body);
+
+    const todo = await prisma.todo.create({ data: { complete, description } });
+
+    return NextResponse.json(todo);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error, { status: 400 });
+  }
 }
