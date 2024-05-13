1e6a2417d69a265b7b73856703c91ec123561462
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-13 11:47:39 +0200

    feat: PUT route complete

--- 03-admin-todos/src/app/api/todos/[id]/route.ts
+++ 03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/app/lib/prisma';
 import { NextResponse } from 'next/server';
+import * as yup from 'yup';
 
 export async function GET(
   request: Request,
@@ -13,4 +14,32 @@ export async function GET(
     return NextResponse.json({ message: 'Todo not found' }, { status: 404 });
 
   return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional().default(false),
+});
+
+export async function PUT(
+  request: Request,
+  segments: { params: { id: string } }
+) {
+  const body = await request.json();
+  console.log({ body });
+  const { id } = segments.params;
+
+  const todo = await prisma.todo.findUnique({ where: { id } });
+
+  if (!todo)
+    return NextResponse.json({ message: 'Todo not found' }, { status: 404 });
+
+  const { description, complete } = await putSchema.validate(body);
+
+  await prisma.todo.update({
+    where: { id },
+    data: { description, complete },
+  });
+
+  return NextResponse.json({ message: `Todo with id: ${id} updated.` });
 }
