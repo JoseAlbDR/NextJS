55ffa978055cfbda38f67aa1dc4c977b30763122
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-14 09:48:51 +0200

    feat: add and remove products from cart with cookies done

--- 03-admin-todos/src/components/TopMenu.tsx
+++ 03-admin-todos/src/components/TopMenu.tsx
@@ -1,7 +1,30 @@
+import { cookies } from 'next/headers';
 import React from 'react';
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch } from 'react-icons/ci';
+import {
+  CiBellOn,
+  CiChat1,
+  CiMenuBurger,
+  CiSearch,
+  CiShoppingBasket,
+} from 'react-icons/ci';
 
 const TopMenu = () => {
+  const cookieStore = cookies();
+  const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}') as {
+    [id: string]: number;
+  };
+
+  const getTotalCount = () => {
+    let items = 0;
+    Object.values(cart).forEach((value) => {
+      items += value;
+    });
+
+    return items;
+  };
+
+  const cartCount = Object.values(cart).reduce((a, b) => a + b, 0);
+
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
@@ -33,8 +56,11 @@ const TopMenu = () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <CiBellOn size={25} />
+          <button className="p-2 flex items-center justify-center  h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+            <span className="text-sm mr-2 text-blue-500 font-bold">
+              {getTotalCount()}
+            </span>
+            <CiShoppingBasket size={25} />
           </button>
         </div>
       </div>
---
+++ 03-admin-todos/src/shopping-cart/actions/actions.ts
@@ -0,0 +1,36 @@
+import { getCookie, hasCookie, setCookie } from 'cookies-next';
+import { Product } from '../../cart/data/products';
+
+export const getCookieCart = (): { [id: string]: number } => {
+  if (hasCookie('cart')) {
+    const cookieCart = JSON.parse((getCookie('cart') as string) ?? '{}');
+    return cookieCart;
+  }
+
+  return {};
+};
+
+export const addProductToCart = (id: string) => {
+  const cookieCart = getCookieCart();
+  if (cookieCart[id]) {
+    cookieCart[id] += 1;
+  } else {
+    cookieCart[id] = 1;
+  }
+
+  setCookie('cart', JSON.stringify(cookieCart));
+};
+
+export const removeProductFromCart = (id: string) => {
+  const cookieCart = getCookieCart();
+
+  let updatedCart: { [key: string]: number } = {};
+  if (cookieCart[id]) {
+    Object.keys(cookieCart).forEach((key) => {
+      if (key === id) return;
+      updatedCart = { ...updatedCart, [key]: cookieCart[key] };
+    });
+
+    setCookie('cart', JSON.stringify(updatedCart));
+  }
+};
