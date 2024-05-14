5895e1179fcef49e1dd267c35666f2a11230cd7a
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-14 16:20:49 +0200

    feat: login with github and google and user card

--- 03-admin-todos/src/app/dashboard/page.tsx
+++ 03-admin-todos/src/app/dashboard/page.tsx
@@ -1,11 +1,25 @@
 import { WidgetItem } from '@/components';
 import React from 'react';
 import SignIn from './components/SignIn';
+import { auth } from '../../../auth';
+import { redirect } from 'next/navigation';
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <SignIn />
+      <WidgetItem title="Usuario conectado S-Side">
+        <div className="flex flex-col">
+          <span>{session?.user?.name}</span>
+          <span>{session?.user?.email}</span>
+          <span>{session?.user?.image}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 };
