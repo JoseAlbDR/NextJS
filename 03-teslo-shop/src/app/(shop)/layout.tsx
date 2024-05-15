b6fc0541830d9f60ad1c327c71b5ff4edc459df1
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-15 23:49:28 +0200

    feat: Sidebar menu items

--- 03-teslo-shop/src/app/(shop)/layout.tsx
+++ 03-teslo-shop/src/app/(shop)/layout.tsx
@@ -1,10 +1,11 @@
-import { TopMenu } from '@/components';
+import { Sidebar, TopMenu } from '@/components';
 import React, { PropsWithChildren } from 'react';
 
 const ShopLayout = ({ children }: PropsWithChildren) => {
   return (
     <main className="min-h-screen">
       <TopMenu />
+      <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
--- 03-teslo-shop/src/components/index.ts
+++ 03-teslo-shop/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as NotFound } from './ui/not-found/PageNotFound';
 export { default as Title } from './ui/title/Title';
 export { default as ProductGrid } from './products/product-grid/ProductGrid';
 export { default as ProductGridItem } from './products/product-grid/ProductGridItem';
+export { default as Sidebar } from './ui/sidebar/Sidebar';
