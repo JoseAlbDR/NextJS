dd5ade9032430181f7806aa43fbaec22c9a62537
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-05-15 12:35:33 +0200

    feat: ProductGrid component

--- 03-teslo-shop/src/components/index.ts
+++ 03-teslo-shop/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as TopMenu } from './ui/top-menu/TopMenu';
 export { default as NotFound } from './ui/not-found/PageNotFound';
 export { default as Title } from './ui/title/Title';
+export { default as ProductGrid } from './products/product-grid/ProductGrid';
