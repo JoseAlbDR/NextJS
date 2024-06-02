2326a582f1da36d0884944d6f9a08d5e87b1ac4d
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-06-02 16:46:39 +0200

    feat: PaypalButtons skeleton

--- 04-teslo-shop/src/components/paypal/PaypalButton.tsx
+++ 04-teslo-shop/src/components/paypal/PaypalButton.tsx
@@ -1,9 +1,18 @@
 'use client';
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import React from 'react';
 
 const PaypalButton = () => {
-  return <PayPalButtons />;
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  return isPending ? (
+    <div className="animate-pulse flex flex-col gap-3">
+      <div className="h-12 bg-gray-300 rounded"></div>
+      <div className="h-12 bg-gray-300 rounded"></div>
+    </div>
+  ) : (
+    <PayPalButtons />
+  );
 };
 
 export default PaypalButton;
